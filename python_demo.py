742eda9b01b57e40f8e1855ec83bd09d3b04f767
Piter <vbaapp@outlook.com>
2024-04-05 22:58:43 +0200

    add demo

---
+++ python_demo.py
@@ -0,0 +1,9 @@
+'''This is my module.
+'''
+i: int = 5
+s: str = 'ala'
+
+if __name__ == '__main__':
+    c: float = 0.3
+    print(i)    
+    print(s)
---
+++ python_demo2.py
@@ -0,0 +1,8 @@
+import python_demo
+
+def main():
+    print(f'Value of i+1 is {python_demo.i}')
+    #print(python_demo.c)
+
+if __name__ == '__main__':
+    main() 
